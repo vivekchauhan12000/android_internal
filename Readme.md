88990111dfa3e29c1ffd348b0c64ae155d9a2908
vivekchauhan12000 <49834398+vivekchauhan12000@users.noreply.github.com>
2021-07-07 11:10:06 +0530

    updated

---
+++ Readme.md
@@ -0,0 +1,18 @@
+### Q2) toast message output
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
+![image](https://user-images.githubusercontent.com/42930642/124701976-416b8100-df0f-11eb-8032-f3f5a23f0d6b.png)
+![image](https://user-images.githubusercontent.com/42930642/124702039-64963080-df0f-11eb-8c4f-3352b2148d19.png)
+
+
+<br/>
+<br/>
+<br/>
+
+### Q3) What is Intent in android?
+
---
+++ app/src/main/java/com/example/toast/MainActivity.java
@@ -0,0 +1,25 @@
+//code by Aditya Kumar Jha
+package com.example.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button)findViewById(R.id.btnShow);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "CONTACT DELETED", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
